f35abd1c97c59944281789caafd69ed779aef6ab
imankns70 <imansoloki@gmail.com>
2019-11-07 13:06:59 +0330

    add api and mvc

---
+++ ActivityManagement.ViewModels/SiteSettings/SiteSettings.cs
@@ -0,0 +1,36 @@
+﻿namespace ActivityManagement.ViewModels.Settings
+{
+    public class SiteSettings
+    {
+        public AdminUserSeed AdminUserSeed { get; set; }
+        public SiteInformation SiteInformation { get; set; }
+        public SiteEmail SiteEmail { get; set; }
+    }
+
+
+    public class AdminUserSeed
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+        public string RoleName { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+
+    public class SiteInformation
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Logo { get; set; }
+        public string Favicon { get; set; }
+    }
+    public class SiteEmail
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+        public string Host { get; set; }
+        public string Port { get; set; }
+    }
+}
