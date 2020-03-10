4e71ef7b78b2a2ccf7a88f60c5acdc4fe1d6fc25
imankns70 <imansoloki@gmail.com>
2020-03-10 23:38:40 +0330

    some changes

---
+++ ActivityManagement.DataLayer/Migrations/20200310153120_UpdateAppUserTable.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ActivityManagement.DataLayer.Migrations
+{
+    public partial class UpdateAppUserTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "RegisterDateTime",
+                table: "AppUsers",
+                nullable: true,
+                defaultValueSql: "CONVERT(datetime,GetDate())",
+                oldClrType: typeof(DateTime),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "IsActive",
+                table: "AppUsers",
+                nullable: false,
+                defaultValueSql: "1",
+                oldClrType: typeof(bool));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "RegisterDateTime",
+                table: "AppUsers",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldNullable: true,
+                oldDefaultValueSql: "CONVERT(datetime,GetDate())");
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "IsActive",
+                table: "AppUsers",
+                nullable: false,
+                oldClrType: typeof(bool),
+                oldDefaultValueSql: "1");
+        }
+    }
+}
