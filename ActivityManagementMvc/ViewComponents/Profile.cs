137f152d7d30553ea987283937a0a1e9af6422b1
imankns70 <imansoloki@gmail.com>
2020-03-17 21:04:29 +0330

    new face dashboard

---
+++ ActivityManagementMvc/ViewComponents/Profile.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ActivityManagementMvc.ViewComponents
+{
+    public class Profile : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View();
+        }
+        
+    }
+}
