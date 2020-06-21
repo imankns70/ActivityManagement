8b872d883287728a4fc7c721cf1d9f6934a9e98f
imankns70 <imansoloki@gmail.com>
2020-06-22 00:27:35 +0430

    add interface to angular

---
+++ ActivityManagementClient/src/app/models/user.ts
@@ -0,0 +1,13 @@
+export interface User {
+
+    id:number,
+    userName:string,
+    firstName:string,
+    lastName:string,
+    phoneNumber:number,
+    email:string,
+    imageUrl:string,
+    birthDate:string,
+    gender:string,
+    role:string,
+}
