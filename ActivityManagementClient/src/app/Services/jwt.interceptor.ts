1d6644a083bef9f2aa6b2c77a5c315ffcfe852e2
iman solouki <imansolouki@outlook.com>
2020-10-13 15:52:08 +0330

    s

---
+++ ActivityManagementClient/src/app/Services/jwt.interceptor.ts
@@ -0,0 +1,29 @@
+import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
+import { Injectable } from "@angular/core";
+import { BehaviorSubject, Observable } from 'rxjs';
+import { AuthService } from '../components/auth/services/auth.service';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class JwtInterCeptor implements HttpInterceptor {
+
+    private isTokenRefreshing = false;
+    tokenSubject = new BehaviorSubject<string>(null);
+
+    constructor(private authService: AuthService) {
+
+    }
+    intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+        return next.handle(this.attachTokenToRequest(req))
+    }
+
+    private attachTokenToRequest(req: HttpRequest<any>) {
+        return req.clone({
+            setHeaders: {
+
+                'Authorization': `Bearer ${this.authService.getToken()}`,
+            },
+        })
+    }
+}
